e28c0733a0db624a4a984b66adcdf463bba3e5f7
rajat0421 <rajattalekar5143@gmail.com>
2024-03-13 17:53:37 +0530

    done

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Management System in PHP | Developed By Rajat , Shreyas</p>
+        </div>
+</footer>
